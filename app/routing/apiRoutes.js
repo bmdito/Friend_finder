7103b9ac0e895c3a080583f812efacb355f0cec8
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-03-21 16:32:48 -0700

    a few more updates

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,14 +10,45 @@ module.exports = function(app){
     app.post("/api/friends", function(req, res){
         // friendData.push(req.body);
         console.log(req.body);
-        var newfriend = req.body.scores
-        var diff = '';
-        for(var i = 0; i<newfriend.length; i++){
-            newfriend[i] - friendData[0].scores[i]
+        var bestMatch = {
+            name:'',
+            photo: '',
+            friendDiff : 1000
+        };
+       
+        // parse new users posts
+        var newFriend = req.body;
+        var newFriendScore = req.body.scores;
 
+        var userName = newFriend.name;
+        var userPhoto = newFriend.photo;
+
+        // use this to calculate difference between survey answers between friends
+        var totalDiff = 0;
+
+        for(var i=0; i<friendData.length; i++){
+            totalDiff = 0;
+            
+            
+            for(var j = 0; j<10; j++){
+            totalDiff += Math.abs(parseInt(newFriendScore[j]) - parseInt(friendData[i].scores[i]));
+            
+            // if the difference is less than the diff between last friend
+                if(totalDiff < bestMatch.friendDiff){
+                //   update best match with that friends data      
+                    bestMatch.name = friendData[i].name;
+                    bestMatch.photo = friendData[i].photo;
+                    bestMatch.friendDiff = totalDiff;
+                }
+            }
+        
         }
-        res.json(friendData[0]);
+
+        // pushes the newly entered survey to the database
+        friendData.push(newFriend);
+
+        res.json(bestMatch);
     });
 
-}
+};
 
